4e5a90d7f9288cc825bb3f0b9f117311a3566ff4
mrssilvana <martha.marisa@yahoo,com>
2021-11-16 14:43:18 +0700

    add file index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head><title>Git Remote</title></head>
+    <body>
+        <h3>Tes Git</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aspernatur consequatur exercitationem libero ducimus quos culpa, suscipit adipisci vitae. Labore, ea laborum dignissimos adipisci facere ab laboriosam? Placeat, illo perferendis?</p>
+    </body>
+</html>
