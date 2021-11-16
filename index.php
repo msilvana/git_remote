850f2e0fe2f4ba437d1d5fc56bd3c782673b06bb
mrssilvana <martha.marisa@yahoo,com>
2021-11-16 14:44:07 +0700

    edit file php menambah paragraf

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head><title>Git Remote</title></head>
     <body>
         <h3>Tes Git</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aspernatur consequatur exercitationem libero ducimus quos culpa, suscipit adipisci vitae. Labore, ea laborum dignissimos adipisci facere ab laboriosam? Placeat, illo perferendis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+        <p>Magni aspernatur consequatur exercitationem libero ducimus quos culpa, suscipit adipisci vitae. Labore, ea laborum dignissimos adipisci facere ab laboriosam? Placeat, illo perferendis?</p>
     </body>
 </html>
