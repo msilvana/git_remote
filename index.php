210a41af3aa24755f5d81e51204ceea1938ae9a1
mrssilvana <martha.marisa@yahoo,com>
2021-11-16 14:50:09 +0700

    menambah list

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
         <h3>Tes Git</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
         <p>Magni aspernatur consequatur exercitationem libero ducimus quos culpa, suscipit adipisci vitae. Labore, ea laborum dignissimos adipisci facere ab laboriosam? Placeat, illo perferendis?</p>
+        <ul><li>Coba 1</li></ul>
     </body>
 </html>
