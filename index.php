647d554a8db8af2a55306db69bbacf6e8604a34c
mrssilvana <martha.marisa@yahoo,com>
2021-11-30 09:36:39 +0700

    menambahkan script js fundamental 2

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head><title>Git Remote</title></head>
-    <body>
-        <h3>Tes Git</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <p>Magni aspernatur consequatur exercitationem libero ducimus quos culpa, suscipit adipisci vitae. Labore, ea laborum dignissimos adipisci facere ab laboriosam? Placeat, illo perferendis?</p>
-        <ul><li>Coba 1</li></ul>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
 </html>
